a86eb315764391b53d163be1c291812b21150844
Daniele <daniele.germani.85@gmail.com>
2018-01-24 03:33:49 +0100

    added scss and img folder , working in scss files and html

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -25,35 +25,45 @@ export class WeatherComponent implements OnInit {
 
     console.log(this.items)
   }
+
   choiceColor(data){
-    for (let i = 0; i <= this.forecast.length - 1; i = i + 7) {
+    for (let i = 1; i <= this.forecast.length - 1; i = i + 7) {
       let item = [];
       let date = data.list[i].dt_txt;
       let code = data.list[i].weather[0].id;
       let temp = data.list[i].main.temp;
       let color;
       let border;
+      let dayOrNigth;
+
+
+      if(data.list[i].sys.pod == 'n'){
+        dayOrNigth = 'night';
+      }else{
+        dayOrNigth = 'day';
+      }
 
       if (code == 800) {
-        color = 'rgba(255, 246, 0, 0.3)';
+        color = 'rgba(255, 246, 0, 0.1)';
         border = '3px solid rgb(255, 246, 0)';
       } if (code == 500 || code == 501 || code == 502 || code == 503) {
-        color = 'rgba(0, 210, 255, 0.3)';
+        color = 'rgba(0, 210, 255, 0.1)';
         border = '3px solid rgb(0, 210, 255)';
       } if (code == 600 || code == 601 || code == 602 || code == 615) {
-        color = 'rgba(231, 76, 60, 0.3)';
+        color = 'rgba(231, 76, 60, 0.1)';
         border = '3px solid rgb(231, 76, 60)';
-      } if (code == 801 || code == 802 || code == 803) {
-        color = 'rgba(255, 255, 255, 0.3)';
+      } if (code == 801 || code == 802 || code == 803 || code == 804) {
+        color = 'rgba(255, 255, 255, 0.1)';
         border = '3px solid rgb(255, 255, 255)';
       } if (code == 900 || code == 901 || code == 903 || code == 904) {
-        color = 'rgba(23, 32, 42, 0.3)';
+        color = 'rgba(23, 32, 42, 0.1)';
         border = '3px solid rgb(23, 32, 42)';
       } if (code == 300 || code == 300 || code == 301 || code == 310) {
-        color = 'rgba(40, 180, 99, 0.3)';
+        color = 'rgba(40, 180, 99, 0.1)';
         border = '3px solid rgb(40, 180, 99)';
       }
-      item.push(date, code, temp, color,border)
+
+      item.push(date, code, temp, color,border,dayOrNigth)
       // console.log(item)
       this.items.push(item)
     }
